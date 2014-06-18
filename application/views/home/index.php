c58dee5388eb9f403ef7abf9228bdec39abef17a
robertgabriel <robertdinu@cronos-technologies.com>
2014-06-18 15:00:54 +0300

    Jquery and app.js

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,18 +1,19 @@
+<div class="col-md-12">
+    <table>
+        <thead>
+        <tr>
+            <td>1</td><td>1</td>
+        </tr>
 
-<table>
-    <thead>
-    <tr>
-        <td>1</td><td>1</td>
-    </tr>
-
-    </thead>
-    <tbody>
-    <tr>
-        <td>2</td><td>2</td>
-    </tr>
-    <tr>
-        <td>2</td><td>2</td>
-    </tr>
-    </tbody>
-</table>
-<?php echo 'Data in index: ' ; var_dump($data); ?>
+        </thead>
+        <tbody>
+        <tr>
+            <td>2</td><td>2</td>
+        </tr>
+        <tr>
+            <td>2</td><td>2</td>
+        </tr>
+        </tbody>
+    </table>
+    <?php echo 'Data in index: ' ; var_dump($data); ?>
+</div>
--- application/views/layouts/standard.php
+++ application/views/layouts/standard.php
@@ -1,13 +1,41 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" href="css/bootstrap.css"/>
-    <link rel="stylesheet" href="css/bootstrap-theme.css"/>
-    <script type="text/javascript" src="js/jquery-1.11.1.js" ></script>
-    <script type="text/javascript" src="js/bootstrap.min.js" ></script>
-    <title>Smooth</title>
-</head>
-<body>
-<?php echo $content; ?>
-</body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Smooth</title>
+        <link rel="stylesheet" href="css/bootstrap.css"/>
+        <link rel="stylesheet" href="css/bootstrap-theme.css"/>
+        <script type="text/javascript" src="js/jquery-1.11.1.js" ></script>
+        <script type="text/javascript" src="js/bootstrap.min.js" ></script>
+        <script type="text/javascript" src="js/app.js" ></script>
+        <!--[if gte IE 9]>
+          <style type="text/css">
+            .gradient {
+               filter: none;
+            }
+          </style>
+        <![endif]-->
+    </head>
+    <body>
+    <div class="big-container">
+        <header>
+
+        </header>
+        <div class="side-panel">
+            <ul>
+                <li>Home</li>
+                <li>Users</li>
+                <li>Logout</li>
+            </ul>
+
+        </div>
+        <div class="row">
+            <section>
+            <?php echo $content; ?>
+            </section>
+        </div>
+    </div>
+
+    </body>
 </html>
