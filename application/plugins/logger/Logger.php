da59aac5e0d82d0d4ed9f81754a8a89202b634b9
robertgabriel <robertdinu@cronos-technologies.com>
2014-07-04 12:39:26 +0300

    imagine library

--- application/plugins/logger/Logger.php
+++ application/plugins/logger/Logger.php
@@ -2,10 +2,18 @@
 
 class Logger
 {
+    const TYPE_INFO = 0;
+    const TYPE_ERROR = 1;
+
+    protected $_dir;
     protected $_file;
+    protected $_type;
     protected $_entries;
     protected $_start;
     protected $_end;
+    protected $_log_permissions = "0493";
+    protected $_dir_errors = "errors";
+    protected $_info = "info";
 
     protected function _sum($values)
     {
@@ -30,8 +38,17 @@ class Logger
         {
             throw new Exception("Log file invalid.");
         }
-
+        if (!isset($options["dir"]))
+        {
+            throw new Exception("Log dir invalid.");
+        }
+        if (!isset($options["type"]))
+        {
+            throw new Exception("Log dir invalid.");
+        }
+        $this->_dir = $options["dir"];
         $this->_file = $options["file"];
+        $this->_type = $options["type"];
         $this->_entries = array();
         $this->_start = microtime();
     }
@@ -47,6 +64,7 @@ class Logger
     public function __destruct()
     {
         $messages = "";
+        $path = "";
         $last = $this->_start;
         $times = array();
 
@@ -63,6 +81,34 @@ class Logger
         $messages .= ", Total: " . (microtime() - $this->_start);
         $messages .= "\n";
 
-        file_put_contents($this->_file, $messages, FILE_APPEND);
+        $year = date('Y');
+        $month =date('M');
+
+        $this->createDir($this->_dir);
+        $path .= $this->_dir.DIRECTORY_SEPARATOR;
+
+        switch ($this->_type){
+
+            case self::TYPE_INFO:
+                $this->createDir($path.$this->_info);
+                $path .= $this->_info.DIRECTORY_SEPARATOR;
+                break;
+
+            case self::TYPE_ERROR:
+                $this->createDir($path.$this->_dir_errors);
+                $path .= $this->_dir_errors.DIRECTORY_SEPARATOR;
+                break;
+        }
+
+        $this->createDir($path.$year);
+        $path .= $year.DIRECTORY_SEPARATOR;
+        $this->createDir($path.$month);
+        $path .= $month.DIRECTORY_SEPARATOR.$this->_file;
+
+        file_put_contents($path, $messages, FILE_APPEND);
+    }
+
+    protected function createDir($dir){
+        if(!is_dir($dir)) mkdir($dir, $this->_log_permissions);
     }
 }
